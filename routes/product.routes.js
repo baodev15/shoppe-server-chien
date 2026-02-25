82f387fa788652de8dd7778658b142a5faa2bd73
baodev15 <baolerussia@gmail.com>
2026-02-25 16:29:10 +0700

    25_2_3

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -4,17 +4,17 @@ const productController = require('../controllers/product.controller');
 
 router.get('/add', productController.addForAdmin);
 router.get('/check', productController.getForCheckInfo);
-router.get('/info', productController.getProductInfo);
 router.put('/info', productController.updateInfoProduct);
+
 router.get('/create-video', productController.getForCreateVideo);
 router.post('/create-video', productController.updateCreateVideo);
+
 router.get('/upload-video', productController.getForUploadVideo);
 router.post('/upload-video', productController.updateUploadVideo);
 
+
 router.post('/', productController.createProduct);
 router.get('/', productController.getAllProducts);
-
-// Routes with ID parameter should be last to avoid conflicts
 router.get('/:id', productController.getProductById);
 router.put('/:id', productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
